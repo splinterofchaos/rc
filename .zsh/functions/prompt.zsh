78ccde0f3411e5696fb3a0b5a49cc5656f9eb516
Scott Prager <splinterofchaos@gmail.com>
2012-07-07 18:11:55 -0400

    Add config: tmux, xmonad, nautilus, uzbl
    Signed-off-by: Scott Prager <splinterofchaos@gmail.com>

--- .zsh/functions/prompt.zsh
+++ .zsh/functions/prompt.zsh
@@ -18,15 +18,15 @@ CYN=$PR_CYAN
 # %R - repository path
 # %S - path in the repository
 # %s - the svn client
-FMT_BRANCH="${YEL}(%s)${NO} ${RED}%r${NO}::${YEL}%b${RED}%u${BLU}%c" # i.e. (git) base-dir::master[unstaged][staged]
+FMT_BRANCH="${YEL}(%r)${NO} ${RED}%s${NO}::${YEL}%b${RED}%u${BLU}%c" # i.e. (base-dur) vcs::master[unstaged][staged]
 FMT_ACTION="${NO}(${CYN}%a${NO}%)"   # e.g. (rebase-i)
 FMT_PATH="%R${YEL}/%S"              # e.g. ~/repo/subdir
 
 # check-for-changes can be really slow.
 # you should disable it, if you work with large repositories    
 zstyle ':vcs_info:*:prompt:*' check-for-changes true
-zstyle ':vcs_info:*:prompt:*' unstagedstr '!!!'  # display !!! if there are unstaged changes
-zstyle ':vcs_info:*:prompt:*' stagedstr '*'    # display * if there are staged changes
+zstyle ':vcs_info:*:prompt:*' unstagedstr '*'  # display if there are unstaged changes
+zstyle ':vcs_info:*:prompt:*' stagedstr   '+'  # display if there are staged changes
 zstyle ':vcs_info:*:prompt:*' actionformats "${FMT_BRANCH}${FMT_ACTION}" "${FMT_PATH}"
 zstyle ':vcs_info:*:prompt:*' formats       "${FMT_BRANCH}"              "${FMT_PATH}"
 zstyle ':vcs_info:*:prompt:*' nvcsformats   ""                             "%~"        
@@ -47,3 +47,21 @@ function rprompt {
  
 lprompt
 rprompt
+
+
+# give us some awesome colour stuffs
+autoload colors zsh/terminfo
+
+# style the git messages
+zstyle ':vcs_info:*:prompt:*' enable git svn darcs hg
+zstyle ':vcs_info:*:prompt:*' check-for-changes true
+zstyle ':vcs_info:*:prompt:*' use-prompt-escapes true
+zstyle ':vcs_info:*:prompt:*' stagedstr '%F{green}OK! %f'
+zstyle ':vcs_info:*:prompt:*' unstagedstr '%F{red} * %f'
+zstyle ':vcs_info:*:prompt:*' actionformats "(%s:%b%u%c - %a)" "[%R/%S]"
+zstyle ':vcs_info:*:prompt:*' formats       "%F{green}%s%f:%F{14}%b%f%u%c" "[%R/%S]"
+zstyle ':vcs_info:git:prompt:*' formats     "%F{green}git%f:%F{blue}%b%f%u%c" "[%R/%S]"
+zstyle ':vcs_info:*:prompt:*' nvcsformats ""
+
+# set the prompt to show git stuff!
+RPROMPT='%B${vcs_info_msg_0_}$b'
